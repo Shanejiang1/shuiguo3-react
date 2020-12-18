5bf22c3a6f7d5dbaf21a5abcfbe31e7432dc566b
Shanejiang1 <jiangshanghang@gmail.com>
2020-12-18 20:21:03 +0800

    Money.tsx 的 数字面板 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -63,7 +63,36 @@ const CategorySection = styled.section`
   }
 `;
 const NumberPadSection = styled.section`
-
+  display: flex;
+  flex-direction: column;
+  > .output{
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25), inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{
+  
+    > button{
+      font-size: 20px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: 4px solid #f5f5f5;
+      background: #ffffff;
+      
+      &.ok{
+        height: 192px;
+        float: right;
+        background: #3775f6;
+        color: #ffffff;
+      }
+      &.zero{
+        width: 50%;
+      }
+    }
+  }
 `;
 
 
@@ -92,10 +121,10 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>
+        <div className="output">
           100
         </div>
-        <div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -103,11 +132,11 @@ function Money() {
           <button>4</button>
           <button>5</button>
           <button>6</button>
+          <button className="ok">完成</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>ok</button>
-          <button>0</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
