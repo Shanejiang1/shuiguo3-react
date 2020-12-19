0be59e1075970c1db9cd2e22e67751adc642a180
Shanejiang1 <jiangshanghang@gmail.com>
2020-12-19 22:58:47 +0800

    收集表单数据

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -33,9 +33,14 @@ const Wrapper = styled.section`
     margin-top: 8px;
   }
 `;
-const TagsSection: React.FC = (props) => {
+
+type Props = {
+  value: string[];
+  onChange: (selected: string[]) => void;
+}
+const TagsSection: React.FC<Props> = (props) => {
   const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const selectedTags = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('请输入标签名称');
     if (tagName !== null) {
@@ -46,12 +51,12 @@ const TagsSection: React.FC = (props) => {
     const index = selectedTags.indexOf(tag);
     if (index >= 0) {
       // 如果 tag 已经被选中，就复制所有没有被选中的 tag 作为新的 selectedTag
-      setSelectedTags(selectedTags.filter(t => t !== tag));
+      props.onChange(selectedTags.filter(t => t !== tag));
     } else {
-      setSelectedTags([...selectedTags, tag]);
+      props.onChange([...selectedTags, tag]);
     }
   };
-  const getClass = (tag:string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
   return (
     <Wrapper>
       <ol>
