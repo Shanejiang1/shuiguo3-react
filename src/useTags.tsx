10e25bca4f52bd3377fccf9c9675e6b3dfc2988d
Shanejiang1 <jiangshanghang@gmail.com>
2020-12-20 22:04:27 +0800

    删除 tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -20,21 +20,31 @@ const useTags = () => {
     }
     return result;
   };
-  const updateTag = (id:number, obj:{name: string}) => {
+  const updateTag = (id: number, obj: { name: string }) => {
     // 获取要改的 tag 的下标
     const index = findTagIndex(id);
     // 深拷贝 tags 得到 tagsClone
     const tagsClone = JSON.parse(JSON.stringify(tags));
     // 把 tagsClone 的第 index 删掉，换成 {id:id, name:obj.name}
-    tagsClone.splice(index, 1, {id:id, name:obj.name});
+    tagsClone.splice(index, 1, {id: id, name: obj.name});
     setTags(tagsClone);
-  }
+  };
+  const deleteTag = (id: number) => {
+    // 获取要删的 tag 的下标
+    const index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    // 把 tagsClone 的第 index 删掉
+    tagsClone.splice(index, 1);
+    setTags(tagsClone);
+  };
   return {
     tags: tags,
     setTags: setTags,
     findTag,
     updateTag,
-    findTagIndex
+    findTagIndex,
+    deleteTag
   };
 };
 
