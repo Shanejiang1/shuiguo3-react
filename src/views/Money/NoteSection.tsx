13452feff177cd67f1aedf9c946e97091651822f
Shanejiang1 <jiangshanghang@gmail.com>
2020-12-20 20:58:02 +0800

    创建单独的 Button 组件

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,24 +1,10 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   padding: 0 16px;
   font-size: 14px;
-  > label{
-    display: flex;
-    align-items: center;
-    > span{
-    margin-right: 16px;
-    white-space: nowrap;
-    }
-    > input{
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -26,23 +12,13 @@ type Props = {
   onChange: (value: string) => void;
 }
 const NoteSection: React.FC<Props> = (props) => {
-  const note = props.value
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
+  const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
   };
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text"
-               placeholder="请添加备注"
-               ref={refInput}
-               defaultValue={note}
-               onBlur={onBlur}/>
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange}/>
     </Wrapper>
   );
 };
