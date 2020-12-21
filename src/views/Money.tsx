9f33a7a8231d7e653fc11d8ffceb9f906e93f58b
Shanejiang1 <jiangshanghang@gmail.com>
2020-12-21 21:39:52 +0800

    解决无法输入 点 的 bug

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {TagsSection} from './Money/TagsSection';
 import {NoteSection} from './Money/NoteSection';
@@ -36,6 +36,12 @@ function Money() {
       setSelected(defaultFormData);
     }
   };
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('时间到')
+      setSelected({...selected, amount: 1000});
+    }, 3000);
+  }, []);
   return (
     <MyLayout>
       <TagsSection value={selected.tagIds}
